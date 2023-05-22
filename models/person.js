9030ae200092cd449e465bb94923e13a9e99c79b
matiasberglund <matias.berglund@hotmail.com>
2023-05-22 16:16:48 +0300

    validation done for phonebook, frontend built

--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 })
 
 personSchema.set('toJSON', {
